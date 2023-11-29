5b406ae7d8ba9f6b3f32367e29a3cf567332281a
Antonio Mastromarino <antonio.v.mastromarino@gmail.com>
2023-11-29 14:29:44 +0000

    tests_written_bugs_to_fix

--- epios/__init__.py
+++ epios/__init__.py
@@ -1,3 +1,4 @@
 from .age_region import Sampler # noqa
 from .non_responders import NonResponder # noqa
 from .data_process import DataProcess # noqa
+from .sampling_maker import SamplingMaker # noqa
