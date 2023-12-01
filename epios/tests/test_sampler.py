70e870b1153ac71c5709fdeb134097619f6dcb1e
Abbie Evans <abbieevans75@gmail.com>
2023-12-01 23:26:27 +0000

    Import update

--- epios/tests/test_sampler.py
+++ epios/tests/test_sampler.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import unittest
 from unittest import TestCase
-from sampler import Sampler
+from epios.sampler import Sampler
 import os
 from pandas.testing import assert_frame_equal
 from epios.utils import person_allowed
