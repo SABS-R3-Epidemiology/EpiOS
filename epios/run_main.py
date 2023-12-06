48df31d23abe4aa31e95a8d38a141a50e79c8b13
Tom Reed <wolf7144@ox.ac.uk>
2023-12-06 14:11:26 +0000

    Basic script set up

--- epios/run_main.py
+++ epios/run_main.py
@@ -1,7 +1,25 @@
 import params
-import random
 from sampler import Sampler
 
-count = 0
+# Sampler Instance
+SAMPLE = Sampler(num_age_group=2, data_path="./input/data.csv")
 
-sample = Sampler(num_age_group=5, data_path="ih_status_output.csv")
+# Sample over age and region
+choice = SAMPLE.sample(6)
+
+# Length of age/region sample
+original_sample_length = len(choice)
+
+# Shrink sample according to household limits
+new_sample = []
+for s in choice:
+
+    if SAMPLE.person_allowed(new_sample, str(s), threshold=params.household_threshold):
+
+        new_sample.append(str(s))
+
+# Length of new sample
+final_sample_length = len(new_sample)
+
+# Compare these lengths
+print(original_sample_length, final_sample_length)
